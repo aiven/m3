88f6bbd4c03942802d2e104765efb77536e45e6b
Markus Stenberg <mstenber@aiven.io>
2020-06-04 09:03:47 +0300

    aivenify.sh: Made nop (not really needed 0.15+)

--- aivenify.sh
+++ aivenify.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# 0.15.0+ this is not needed. Leaving file around so build steps stay same.
+exit 0
+
 rm -rf vendor
 if [ "$1" == "0" ] ; then
     egrep -lRZ 'github.com\/aiven\/m3' . | xargs -r -0 -l sed -i -e 's/github.com\/aiven\/m3/github.com\/m3db\/m3/g'
